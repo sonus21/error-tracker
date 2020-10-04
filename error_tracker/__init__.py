d4aedaad149c175ff03b0e506642a591d9b2e3ac
Sonu Kumar <sonunitw12@gmail.com>
2020-10-04 21:58:35 +0530

    optional flask issue

--- error_tracker/__init__.py
+++ error_tracker/__init__.py
@@ -6,16 +6,26 @@
 #    :license: BSD-3-Clause
 #
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 __author__ = 'Sonu Kumar'
 __email__ = 'sonunitw12@gmail.com'
 
 from error_tracker.libs.mixins import *
-from error_tracker.flask import *
-from error_tracker.django import *
-from error_tracker.flask.utils import configure_scope as flask_scope
-from error_tracker.django.apps import DjangoErrorTracker
-from error_tracker.django.utils import capture_message, track_exception, configure_scope, capture_exception
+
+try:
+    import flask
+    from error_tracker.flask import *
+    from error_tracker.flask.utils import configure_scope as flask_scope
+except ImportError:
+    pass
+try:
+    import django
+    from error_tracker.django import *
+    from error_tracker.django.apps import DjangoErrorTracker
+    from error_tracker.django.utils import capture_message, track_exception, configure_scope, capture_exception
+except ImportError:
+    pass
+
 from error_tracker.libs.exception_formatter import *
 
 __all__ = [
