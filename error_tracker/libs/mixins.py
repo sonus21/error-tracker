14a376af7471941124890bc22704fb2aeebf8b6e
Thierry BOULOGNE <tboulogne@tng-concepts.com>
2020-10-18 09:00:27 +0200

    update fields

--- error_tracker/libs/mixins.py
+++ error_tracker/libs/mixins.py
@@ -39,6 +39,8 @@ class ModelMixin(object):
     count = None
     created_on = None
     last_seen = None
+    notification_send = None
+    ticket_raised = None
 
     def __str__(self):
         return "'%s' '%s' %s" % (self.host, self.path, self.count)
